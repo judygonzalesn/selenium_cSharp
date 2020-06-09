56635397cb0131e329140ba4ddced18eac1da0db
judylovesthis <judylovethis@gmail.com>
2020-06-09 21:11:09 +0800

    Add files via upload

---
+++ SeleniumCSharpProject/BaseClass/BaseTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace SeleniumCSharpProject.BaseClass
+{
+   public class BaseTest
+    {
+       public IWebDriver driver;
+
+        [SetUp]
+        public void Open() {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            // driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Url = "https://www.phptravels.net/home";
+            
+
+        }
+        [TearDown]
+        public void Close()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumCSharpProject/BaseClass/tablePage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumCSharpProject.BaseClass
+{
+    class tablePage
+    {
+
+        String test_url = "https://www.worldometers.info/coronavirus/#countries";
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        
+
+
+        public tablePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            PageFactory.InitElements(driver, this);
+            
+        }
+        [FindsBy(How = How.CssSelector, Using = "[id='main_table_countries_today']>tbody>tr:nth-of-type(49)>td:nth-of-type(2)")]
+        private IWebElement phElement;
+        
+
+        [FindsBy(How = How.CssSelector, Using = "[id='main_table_countries_today']")]
+        private IWebElement tableElement;
+
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl(test_url);
+        }
+
+        public void verifyTextInside()
+        {
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(phElement);
+            Assert.AreEqual("Philippines", phElement.Text);
+        }
+    }
+}
+
